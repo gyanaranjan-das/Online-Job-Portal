b018e75a75b5b557e8d4b9d4ee4254ce6a9a141d
gyanaranjan-das <dasgyanaranjan835@gmail.com>
2025-10-15 01:12:27 +0530

    Created UserDAO

--- src/main/java/com/jobportal/DAO/UserDAO.java
+++ src/main/java/com/jobportal/DAO/UserDAO.java
@@ -1,4 +1,5 @@
-package com.jobportal.DAO;
+package com.jobportal.dao;
+
 import com.jobportal.model.User;
 import com.jobportal.util.DatabaseConnection;
 
@@ -6,6 +7,47 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-public class UserDAO {
 
+public class UserDAO{
+    public boolean registerUser(User user){
+        String sql = "INSERT INTO users (email,password,full_name.role) VALUES(?,?,?,?)";
+        try (Connection conn = DatabaseConnection.getConnection();
+        PreparedStatement pstmt = conn.prepareStatement(sql)){
+            pstmt.setString(1, user.getEmail());
+            pstmt.setString(2, user.getPassword());
+            pstmt.setString(3, user.getFullName());
+            pstmt.setString(4, user.getRole());
+
+            int rowsAffected = pstmt.executeUpdate();
+            return rowsAffected>0;
+        }catch(SQLException e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+     public User getUserByEmail(String email) {
+        String sql = "SELECT * FROM users WHERE email = ?";
+        User user = null;
+
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, email);
+            ResultSet rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                user = new User();
+                user.setUserId(rs.getInt("user_id"));
+                user.setEmail(rs.getString("email"));
+                user.setPassword(rs.getString("password"));
+                user.setFullName(rs.getString("full_name"));
+                user.setRole(rs.getString("role"));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return user;
+     }
 }
